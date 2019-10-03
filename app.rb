d7c15eb3ee2ac3d509fa9bc30ed852801ee1c2b7
Christian MICHON <christian.michon@gmail.com>
2019-10-03 21:00:41 +0200

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,30 +1,25 @@
-require 'octokit'
-require 'rest-client'
-require 'sinatra'
-require 'sinatra/reloader'
-require 'yaml'
+%w[octokit rest-client sinatra sinatra/reloader].map &method(:require)
 
-#~ set :bind, '0.0.0.0'
+#~ set :bind, '0.0.0.0' # needed outside heroku
 
 get '/' do
-  #"woot"
   erb :index, :locals => {:client_id => ENV['CLIENT_ID']}
 end
 
 get '/profile' do
   # Retrieve temporary authorization grant code
-  p request.env
   session_code = request.env['rack.request.query_hash']['code']
 
-  # POST Auth Grant Code + CLIENT_ID/SECRECT in exchange for our access_token
+  # validating OAuth2 authentication
   response = RestClient.post(
     'https://github.com/login/oauth/access_token',
-    # POST payload
-    { :client_id => ENV['CLIENT_ID'],
+    {
+      :client_id => ENV['CLIENT_ID'],
       :client_secret => ENV['CLIENT_SECRET'],
-      :code => session_code },
-    # Request header for JSON response
-    :accept => :json)
+      :code => session_code
+    },
+    :accept => :json
+  )
 
   # Parse access_token from JSON response
   access_token = JSON.parse(response)['access_token']
@@ -51,25 +46,11 @@ get '/profile' do
   erb :profile, :locals => profile_data
 end
 
-#get '/db' do
-#  DB.to_s
-#end
-
-#get '/json' do
-  #str_body = File.read("file.txt") rescue ""
-  #payload = JSON.parse str_body
-  #p payload['action']
-  #str_body
-  #ENV.to_h.to_yaml
-#end
-
 post '/webhook' do
   str_body = request.body.read
   payload = JSON.parse str_body
   repo = payload["repository"]["full_name"]
   pr_number = payload["number"]
-  #File.open('file.txt','w'){|f| f.write str_body}
-  #event = request.env['HTTP_X_GITHUB_EVENT']
   client = Octokit::Client.new(:access_token => ENV['GITHUB_AUTH_TOKEN'])
   pr = client.pull_request(repo, pr_number)
   pr_login = pr.head.user.login
