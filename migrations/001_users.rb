f95f8f655730099e008ee1c26768fa537c06f81b
Christian MICHON <christian.michon@gmail.com>
2019-10-04 09:01:19 +0200

    Update 001_users.rb

--- migrations/001_users.rb
+++ migrations/001_users.rb
@@ -2,9 +2,7 @@ Sequel.migration do
   change do
     create_table(:users) do
       primary_key :id
-      String :email, null: false
-      String :full_name, null: false
-      String :github_login, unique: true, null: false
+      String :login, unique: true, null: false
     end
   end
 end
