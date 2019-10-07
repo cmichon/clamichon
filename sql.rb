114c221ef00e29c31ea99ceeef1e9e3c9ea57741
Christian MICHON <christian.michon@gmail.com>
2019-10-07 12:27:00 +0200

    Update sql.rb

--- sql.rb
+++ sql.rb
@@ -1,5 +1,5 @@
-require 'irb/completion'
-require 'sequel'
+%w[irb/completion sequel].map &method(:require)
 DB = Sequel.connect ENV['DATABASE_URL']
-class User < Sequel::Model ; end
-class Check < Sequel::Model ; end
+require_relative 'models'
+
+# vim:et:ff=unix:sw=2:ts=2:
