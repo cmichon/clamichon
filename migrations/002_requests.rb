a3c6b5922ff6cbea6a52a9d40ac0d970c1cc7e4c
Christian MICHON <christian.michon@gmail.com>
2019-10-09 09:10:42 +0200

    Update and rename 002_checks.rb to 002_requests.rb

--- migrations/002_requests.rb
+++ migrations/002_requests.rb
@@ -1,11 +1,13 @@
 Sequel.migration do
   change do
-    create_table(:checks) do
+    create_table(:requests) do
       primary_key :id
+      String :status, null: false
       String :login, null: false
       String :repo, null: false
       String :sha, null: false
       index :login
+      index :status
     end
   end
 end
