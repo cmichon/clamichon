52eec84e07f51a35482f3040c46fc413922a970f
Christian MICHON <christian.michon@gmail.com>
2019-10-09 09:03:09 +0200

    Update models.rb

--- models.rb
+++ models.rb
@@ -1,7 +1,10 @@
 class User < Sequel::Model
 end
 
-class Check < Sequel::Model
+class ApprovedRequest < Sequel::Model
+end
+
+class PendingRequest < Sequel::Model
 end
 
 # vim:et:ff=unix:sw=2:ts=2:
