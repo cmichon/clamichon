97046f4ff3b2ae972ae812b7bad30225b1a7c156
Christian MICHON <christian.michon@gmail.com>
2019-10-11 09:36:32 +0200

    Introducing helpers

--- app.rb
+++ app.rb
@@ -1,66 +1,87 @@
+module AppHelpers
+  def request_access_token(code) #{{{
+    @access_response = Faraday.post(
+      'https://github.com/login/oauth/access_token',
+      {
+        client_id: ENV['CLIENT_ID'],
+        client_secret: ENV['CLIENT_SECRET'],
+        code: code
+      },
+      { Accept: 'application/json' }
+    )    
+  end #}}}
+  def login_as_user #{{{
+    access_token = JSON.parse(@access_response.body)['access_token']
+    client = Octokit::Client.new(access_token: access_token)
+    @user = client.user
+    @locals = {
+      login: "#{@user.login}",
+      url: "#{@user.html_url}"
+    } # will fail if access_token is wrong
+  end #}}}
+  def cla_signature #{{{
+    if User.where(login: locals[:login]).count == 1
+      already_signed
+    else
+      initial_signature
+    end
+  end #}}}
+  def already_signed #{{{
+    @locals[:status] = 'Welcome back!'
+  end #}}}
+  def initial_signature #{{{
+    add_new_cla_user
+    remove_pending_state_for_cla_user
+    @locals[:status] = 'Success!'
+  end #}}}
+  def add_new_cla_user #{{{
+    User.insert({
+      login: @user.login,
+      cla_invidual: true,
+      full_name: @user.name,
+      postal_address: "here",
+      country: "any",
+      email: @user.email,
+      phone: "1234567890"
+    })
+  end #}}}
+  def remove_pending_state_for_cla_user #{{{
+    @repo_owner_client = Octokit::Client.new(access_token: ENV['GITHUB_AUTH_TOKEN']) # next ops as repo owner
+    Request.where(login: @locals[:login], status: 'pending').each do |e|
+      @repo_owner_client.create_status(
+        e.repo,
+        e.sha,
+        'success',
+        { context: 'license/cla',
+          description: "Contributor License Agreement signed by @#{@locals[:login]}."
+        }
+      ) rescue nil # we may hit an exception (ex: repo gone), which we clean up next line anyway
+      e.update({status: 'open'})
+    # e.delete
+    end
+  end #}}}
+end
+
 class App < Roda
+  include AppHelpers
   plugin :public
   plugin :render
 
   route do |r|
-
     r.public
 
     r.root do
       render :index, locals: { client_id: ENV['CLIENT_ID'] }
     end
 
-    r.get 'profile' do
-      access_response = Faraday.post(
-        'https://github.com/login/oauth/access_token',
-        {
-          client_id: ENV['CLIENT_ID'],
-          client_secret: ENV['CLIENT_SECRET'],
-          code: r.params['code']
-        },
-        { Accept: 'application/json' }
-      )
-      begin
-        access_token = JSON.parse(access_response.body)['access_token']
-        client = Octokit::Client.new(access_token: access_token) 
-        locals = {
-          login: "#{client.user.login}",
-          url: "#{client.user.html_url}"
-        }
-      rescue # failsafe
-        r.redirect('/')
-      end
-      if User.where(login: locals[:login]).count == 1
-        locals[:status] = 'Welcome back!'
-      else
-        locals[:status] = 'Success!'
-        User.insert({
-          login: client.user.login,
-          cla_invidual: true,
-          full_name: client.user.name,
-          postal_address: "here",
-          country: "any",
-          email: client.user.email,
-          phone: "1234567890"
-        })
-        client = Octokit::Client.new(access_token: ENV['GITHUB_AUTH_TOKEN']) # next ops as repo owner
-        Request.where(login: locals[:login], status: 'pending').each do |e|
-          client.create_status(
-            e.repo,
-            e.sha,
-            'success',
-            { context: 'license/cla',
-              description: "Contributor License Agreement signed by @#{locals[:login]}."
-            }
-          ) rescue nil # we may hit an exception (ex: repo gone), which we clean up next line anyway
-          e.update({status: 'open'})
-        # e.delete
-        end
-      end
-      render :profile, locals: locals
-    end
+    r.get 'profile' do #{{{
+      request_access_token(r.params['code'])
+      login_as_user rescue r.redirect('/') # failsafe
+      cla_signature
+      render :profile, locals: @locals
+    end #}}}
 
-    r.post 'webhook' do
+    r.post 'webhook' do #{{{
       return "" unless request.env['HTTP_X_GITHUB_EVENT'] == "pull_request" # we only accept pull_request
       payload = JSON.parse request.body.read
       repo = payload["repository"]["full_name"]
@@ -101,9 +122,9 @@ class App < Roda
         Request.insert({status: 'pending', login: pr_login, repo: repo, sha: pr.head.sha})
       end
       "webhook processed"
-    end
+    end #}}}
 
   end
 end
 
-# vim:et:ff=unix:sw=2:ts=2:
+# vim:et:ff=unix:sw=2:ts=2:fdm=marker:fdc=2:
