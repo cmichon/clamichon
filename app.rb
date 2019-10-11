be44d893edecb05a744bddd17ff22eccf67a4ed7
Christian MICHON <christian.michon@gmail.com>
2019-10-11 10:29:13 +0200

    Update app.rb

--- app.rb
+++ app.rb
@@ -46,9 +46,9 @@ module AppHelpers
     })
   end #}}}
   def remove_pending_state_for_cla_user #{{{
-    @repo_owner_client = Octokit::Client.new(access_token: ENV['GITHUB_AUTH_TOKEN']) # next ops as repo owner
+    @owner_client = Octokit::Client.new(access_token: ENV['GITHUB_AUTH_TOKEN']) # next ops as repo owner
     Request.where(login: @locals[:login], status: 'pending').each do |e|
-      @repo_owner_client.create_status(
+      @owner_client.create_status(
         e.repo,
         e.sha,
         'success',
@@ -59,6 +59,63 @@ module AppHelpers
       e.update({status: 'open'})
     # e.delete
     end
+  end #}}}
+  def add_branch_protection #{{{
+    @owner_client.protect_branch(
+      @repo,
+      "master",
+      required_status_checks: {
+        strict: false,
+        contexts: [ 'license/cla' ]
+      },
+      enforce_admins: false,
+      required_pull_request_reviews: nil
+    )
+  end #}}}
+  def get_pr_info #{{{
+    @pr_number = @payload["number"]
+    @pr = @owner_client.pull_request(@repo, @pr_number)
+    @pr_login = @pr.head.user.login
+  end #}}}
+  def webhook_logic_by_user #{{{
+    if User.where(login: @pr_login).count == 1
+      webhook_with_signed_cla
+    else
+      webhook_without_signed_cla
+    end
+  end #}}}
+  def webhook_without_signed_cla #{{{
+    @owner_client.create_status(
+      @repo,
+      @pr.head.sha,
+      'pending',
+      { context: 'license/cla',
+        description: 'Contributor License Agreement is not signed yet.',
+        target_url: ENV['CLA_URL']
+      }
+    )
+    @owner_client.add_comment(
+      @repo,
+      @pr_number,
+      'When this pull request was created, the <a href=''' + ENV['CLA_URL'] + '''>Contributor License Agreement</a> was not signed yet.'
+    )
+    Request.insert({status: 'pending', login: @pr_login, repo: @repo, sha: @pr.head.sha})
+  end #}}}
+  def webhook_with_signed_cla #{{{
+    @owner_client.create_status(
+      @repo,
+      @pr.head.sha,
+      'success',
+      { context:'license/cla',
+        description: "Contributor License Agreement signed by @#{@pr_login}."
+      }
+    )
+    @owner_client.add_comment(
+      @repo,
+      @pr_number,
+      'When this pull request was created, the Contributor License Agreement was already signed.'
+    )
+    Request.insert({status: 'open', login: @pr_login, repo: @repo, sha: @pr.head.sha})
   end #}}}
 end
 
@@ -74,55 +131,27 @@ class App < Roda
       render :index, locals: { client_id: ENV['CLIENT_ID'] }
     end
 
-    r.get 'profile' do #{{{
+    r.get 'profile' do
       request_access_token(r.params['code'])
       login_as_user rescue r.redirect('/') # failsafe
       cla_signature
       render :profile, locals: @locals
-    end #}}}
+    end
 
-    r.post 'webhook' do #{{{
-      return "" unless request.env['HTTP_X_GITHUB_EVENT'] == "pull_request" # we only accept pull_request
-      payload = JSON.parse request.body.read
-      repo = payload["repository"]["full_name"]
-      pr_number = payload["number"]
-      client = Octokit::Client.new(access_token: ENV['GITHUB_AUTH_TOKEN'])
-      pr = client.pull_request(repo, pr_number)
-      pr_login = pr.head.user.login
-      if User.where(login: pr_login).count == 1
-        client.create_status(
-          repo,
-          pr.head.sha,
-          'success',
-          { context:'license/cla',
-            description: "Contributor License Agreement signed by @#{pr_login}."
-          }
-        )
-        client.add_comment(
-          repo,
-          pr_number,
-          'When this pull request was created, the Contributor License Agreement was already signed.'
-        )
-        Request.insert({status: 'open', login: pr_login, repo: repo, sha: pr.head.sha})
+    r.post 'webhook' do
+      @payload = JSON.parse request.body.read
+      @repo = @payload["repository"]["full_name"]
+      @owner_client = Octokit::Client.new(access_token: ENV['GITHUB_AUTH_TOKEN'])
+      case request.env['HTTP_X_GITHUB_EVENT']
+      when 'ping'
+        add_branch_protection
+      when 'pull_request'
+        get_pr_info
+        webhook_logic_by_user
       else
-        client.create_status(
-          repo,
-          pr.head.sha,
-          'pending',
-          { context: 'license/cla',
-            description: 'Contributor License Agreement is not signed yet.',
-            target_url: ENV['CLA_URL']
-          }
-        )
-        client.add_comment(
-          repo,
-          pr_number,
-          'When this pull request was created, the <a href=''' + ENV['CLA_URL'] + '''>Contributor License Agreement</a> was not signed yet.'
-        )
-        Request.insert({status: 'pending', login: pr_login, repo: repo, sha: pr.head.sha})
       end
       "webhook processed"
-    end #}}}
+    end
 
   end
 end
