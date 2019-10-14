e06f3c5f0930bf08ede4c91b81c92ec69981b80b
Christian MICHON <christian.michon@gmail.com>
2019-10-14 11:00:55 +0200

    removal of apphelpers

--- app.rb
+++ app.rb
@@ -1,126 +1,4 @@
-module AppHelpers
-  def request_access_token(code) #{{{
-    @access_response = Faraday.post(
-      'https://github.com/login/oauth/access_token',
-      {
-        client_id: ENV['CLIENT_ID'],
-        client_secret: ENV['CLIENT_SECRET'],
-        code: code
-      },
-      { Accept: 'application/json' }
-    )    
-  end #}}}
-  def login_as_user #{{{
-    access_token = JSON.parse(@access_response.body)['access_token']
-    client = Octokit::Client.new(access_token: access_token)
-    @user = client.user
-    @locals = {
-      login: "#{@user.login}",
-      url: "#{@user.html_url}"
-    } # will fail if access_token is wrong
-  end #}}}
-  def cla_signature #{{{
-    if User.where(login: @locals[:login]).count == 1
-      already_signed
-    else
-      initial_signature
-    end
-  end #}}}
-  def already_signed #{{{
-    @locals[:status] = 'Welcome back!'
-  end #}}}
-  def initial_signature #{{{
-    add_new_cla_user
-    remove_pending_state_for_cla_user
-    @locals[:status] = 'Success!'
-  end #}}}
-  def add_new_cla_user #{{{
-    User.insert({
-      login: @user.login,
-      cla_invidual: true,
-      full_name: @user.name,
-      postal_address: "here",
-      country: "any",
-      email: @user.email,
-      phone: "1234567890"
-    })
-  end #}}}
-  def remove_pending_state_for_cla_user #{{{
-    @owner_client = Octokit::Client.new(access_token: ENV['GITHUB_AUTH_TOKEN']) # next ops as repo owner
-    Request.where(login: @locals[:login], status: 'pending').each do |e|
-      @owner_client.create_status(
-        e.repo,
-        e.sha,
-        'success',
-        { context: 'license/cla',
-          description: "Contributor License Agreement signed by @#{@locals[:login]}."
-        }
-      ) rescue nil # we may hit an exception (ex: repo gone), which we clean up next line anyway
-      e.update({status: 'open'})
-    # e.delete
-    end
-  end #}}}
-  def add_branch_protection #{{{
-    @owner_client.protect_branch(
-      @repo,
-      "master",
-      required_status_checks: {
-        strict: false,
-        contexts: [ 'license/cla' ]
-      },
-      enforce_admins: false,
-      required_pull_request_reviews: nil
-    )
-  end #}}}
-  def get_pr_info #{{{
-    @pr_number = @payload["number"]
-    @pr = @owner_client.pull_request(@repo, @pr_number)
-    @pr_login = @pr.head.user.login
-  end #}}}
-  def webhook_logic_by_user #{{{
-    if User.where(login: @pr_login).count == 1
-      webhook_with_signed_cla
-    else
-      webhook_without_signed_cla
-    end
-  end #}}}
-  def webhook_without_signed_cla #{{{
-    @owner_client.create_status(
-      @repo,
-      @pr.head.sha,
-      'pending',
-      { context: 'license/cla',
-        description: 'Contributor License Agreement is not signed yet.',
-        target_url: ENV['CLA_URL']
-      }
-    )
-    @owner_client.add_comment(
-      @repo,
-      @pr_number,
-      'When this pull request was created, the <a href=''' + ENV['CLA_URL'] + '''>Contributor License Agreement</a> was not signed yet.'
-    )
-    Request.insert({status: 'pending', login: @pr_login, repo: @repo, sha: @pr.head.sha})
-  end #}}}
-  def webhook_with_signed_cla #{{{
-    @owner_client.create_status(
-      @repo,
-      @pr.head.sha,
-      'success',
-      { context:'license/cla',
-        description: "Contributor License Agreement signed by @#{@pr_login}."
-      }
-    )
-    @owner_client.add_comment(
-      @repo,
-      @pr_number,
-      'When this pull request was created, the Contributor License Agreement was already signed.'
-    )
-    Request.insert({status: 'open', login: @pr_login, repo: @repo, sha: @pr.head.sha})
-  end #}}}
-end
-
 class App < Roda
-  include AppHelpers
   plugin :public
   plugin :render
 
@@ -132,22 +10,103 @@ class App < Roda
     end
 
     r.get 'profile' do
-      request_access_token(r.params['code'])
-      login_as_user rescue r.redirect('/') # failsafe
-      cla_signature
-      render :profile, locals: @locals
+      access_response = Faraday.post(
+        'https://github.com/login/oauth/access_token',
+        {
+          client_id: ENV['CLIENT_ID'],
+          client_secret: ENV['CLIENT_SECRET'],
+          code: r.params['code']
+        },
+        { Accept: 'application/json' }
+      )    
+      access_token = JSON.parse(access_response.body)['access_token']
+      user_client = Octokit::Client.new(access_token: access_token)
+      user = user_client.user
+      locals = {
+        login: "#{user.login}",
+        url: "#{user.html_url}"
+      } rescue r.redirect('/') # WILL FAIL IF ACCESS_TOKEN IS WRONG
+      if User.where(login: user.login).count == 1 # ALREADY_SIGNED
+        locals[:status] = 'Welcome back!'
+      else # INITIAL_SIGNATURE: ADD NEW CLA USER, RELEASE PENDING SHA1
+        User.insert({
+          login: user.login,
+          cla_invidual: true,
+          full_name: user.name,
+          postal_address: "here",
+          country: "any",
+          email: user.email,
+          phone: "1234567890"
+        })
+        owner_client = Octokit::Client.new(access_token: ENV['GITHUB_AUTH_TOKEN']) # ACT AS REPO OWNER
+        Request.where(login: user.login, status: 'pending').each do |e|
+          owner_client.create_status(
+            e.repo,
+            e.sha,
+            'success',
+            { context: 'license/cla',
+              description: "Contributor License Agreement signed by @#{user.login}."
+            }
+          ) rescue nil # WE MAY HIT AN EXCEPTION (EX: REPO GONE), WHICH WE CLEAN UP NEXT LINE ANYWAY
+          e.update({status: 'open'}) # WAS: e.delete
+        end
+        locals[:status] = 'Success!'
+      end
+      render :profile, locals: locals
     end
 
     r.post 'webhook' do
-      @payload = JSON.parse request.body.read
-      @repo = @payload["repository"]["full_name"]
-      @owner_client = Octokit::Client.new(access_token: ENV['GITHUB_AUTH_TOKEN'])
+      payload = JSON.parse request.body.read
+      repo = payload["repository"]["full_name"]
+      owner_client = Octokit::Client.new(access_token: ENV['GITHUB_AUTH_TOKEN'])
       case request.env['HTTP_X_GITHUB_EVENT']
-      when 'ping'
-        add_branch_protection
-      when 'pull_request'
-        get_pr_info
-        webhook_logic_by_user
+      when 'ping' # {{{ ADD BRANCH PROTECTION
+        owner_client.protect_branch(
+          repo,
+          "master",
+          required_status_checks: {
+            strict: false,
+            contexts: [ 'license/cla' ]
+          },
+          enforce_admins: false,
+          required_pull_request_reviews: nil
+        ) # }}}
+      when 'pull_request' # {{{ CLA BUSINESS LOGIC
+        pr_number = payload["number"]
+        pr = owner_client.pull_request(repo, pr_number)
+        pr_login = pr.head.user.login
+        if User.where(login: pr_login).count == 1 # ALREADY SIGNED CLA
+          owner_client.create_status(
+            repo,
+            pr.head.sha,
+            'success',
+            { context:'license/cla',
+              description: "Contributor License Agreement signed by @#{pr_login}."
+            }
+          )
+          owner_client.add_comment(
+            repo,
+            pr_number,
+            'When this pull request was created, the Contributor License Agreement was already signed.'
+          )
+          Request.insert({status: 'open', login: pr_login, repo: repo, sha: pr.head.sha})
+        else # NO SIGNED CLA
+          owner_client.create_status(
+            repo,
+            pr.head.sha,
+            'pending',
+            { context: 'license/cla',
+              description: 'Contributor License Agreement is not signed yet.',
+              target_url: ENV['CLA_URL']
+            }
+          )
+          owner_client.add_comment(
+            repo,
+            pr_number,
+            'When this pull request was created, the <a href=''' + ENV['CLA_URL'] + '''>Contributor License Agreement</a> was not signed yet.'
+          )
+          Request.insert({status: 'pending', login: pr_login, repo: repo, sha: pr.head.sha})
+        end # }}}
       else
       end
       "webhook processed"
